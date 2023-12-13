359e45ae65802b733fd1e3aa52b2d8b79ddcc01e
soutem-debug <77745136+soutem-debug@users.noreply.github.com>
2023-12-13 16:48:55 +0000

    cleaned the code

--- game.js
+++ game.js
@@ -1,36 +1,34 @@
 
+// Player one rolls 
 
-// var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; 
 
-// var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+var randomImageSource = "assets/images/dice" + randomNumber1 + ".png"; 
 
-// var randomImageSource = "/assets/images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+var image1 = document.querySelectorAll("img")[1];
 
-// var image1 = document.querySelectorAll("img")[0];
-
-// image1.setAttribute("src", randomImageSource);
-
-
-// // Player two rolls 
-
-// var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-
-// var randomDiceImage2 = "dice" + randomNumber1 + ".png";
-
-// var randomImageSource2 = "/assets/images/" + randomNumber2 + randomDiceImage2;
-
-// var image2 = document.querySelectorAll("img")[1];
-
-// image2.setAttribute("src", randomImageSource2);
+image1.setAttribute("src", randomImageSource);
 
 
-// //If player 1 wins
-// if (randomNumber1 > randomNumber2) {
-//   document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
-// }
-// else if (randomNumber2 > randomNumber1) {
-//   document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
-// }
-// else {
-//   document.querySelector("h1").innerHTML = "Draw!";
-// }
+// Player two rolls 
+
+var randomNumber2 = Math.floor(Math.random()* 6) + 1;
+
+var randomImageSource2 = "assets/images/dice" + randomNumber2 + ".png";
+
+var image2 = document.querySelectorAll("img")[2];
+
+image2.setAttribute("src", randomImageSource2);
+
+
+//  The Game 
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🏴‍☠️ Player One Wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+  document.querySelector("h1").innerHTML = "Player Two Wins! 🏴‍☠️";
+}
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
